8feb7ffa265b1f2e06ada962a789823407815da5
Kirazono <66817745+Kirazono@users.noreply.github.com>
2021-02-09 18:18:02 -0800

    Update Solar.js

--- Assignment-5/Solar.js
+++ Assignment-5/Solar.js
@@ -185,7 +185,7 @@ function MakePlanet(ms, name, moons)
     ms.push();
     
     // Actual matrix stuff
-    ms.rotate((1.0 / data.year) * time, [1.0,1.0,1.0]);
+    ms.rotate((1.0 / data.year) * time, [1.0,0.0,1.0]);
     ms.translate(data.distance, 0, 0);
     ms.scale(data.radius);
     
